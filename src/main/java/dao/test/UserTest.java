b6e5ce9ef31c126b53a2b850d35ccc2e196f15d4
Administrator <2416762675@qq.com>
2017-01-06 21:44:49 +0800

    aates

--- src/main/java/dao/test/UserTest.java
+++ src/main/java/dao/test/UserTest.java
@@ -29,9 +29,19 @@ public class UserTest {
          * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
          * getUser是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
          */
-        String statement = "dao.mapper.userMapper.getUser";//映射sql的标识字符串
+        String statement = "dao.entity.UserMapper.selectByPrimaryKey";//映射sql的标识字符串
         //执行查询返回一个唯一user对象的sql
-        User user = session.selectOne(statement, 1);
-        System.out.println(user);
+//        User user = session.selectOne(statement, 1);
+         User user = session.selectOne(statement, 2);
+
+
+
+
+
+
+
+
+
+
     }
 }
